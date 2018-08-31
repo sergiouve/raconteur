0a1a18baed98b14cbbabc7515077621c5629dcca
Sergio Uve <sergiouve@gmail.com>
2018-08-31 14:42:51 +0200

    Add naive game boot validation

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,13 @@
 const Prompter = require('../lib/prompter');
 const Game     = require('../lib/game');
-const tale     = process.argv[2].split('=')[1];
-const game = new Game(tale);
 
-new Prompter(game).start();
+try {
+    const tale = process.argv[2].split('=')[1];
+    const game = new Game(tale);
+
+    new Prompter(game).start();
+} catch (error) {
+    console.log(error);
+    console.log('You must provide a valid installed game');
+    process.exit();
+}
