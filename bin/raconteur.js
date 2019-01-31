4fb22b51991ab9ac7845565b344fa3f1a9f3e321
Sergio Uve <sergiouve@gmail.com>
2019-01-31 17:11:47 +0100

    [WIP] Refactor and cleanup

--- bin/raconteur.js
+++ bin/raconteur.js
@@ -1,13 +1,17 @@
-const Prompter = require('../lib/prompter');
-const Game     = require('../lib/game');
+const Prompter = require('../lib/prompter/prompter')
+const Game     = require('../lib/core/game')
 
 try {
-    const tale = process.argv[2].split('=')[1];
-    const game = new Game(tale);
+    let debugMode
+    const tale = process.argv[2].split('=')[1]
+    const game = new Game(tale)
+    const options = {
+        debug: process.argv[3] === 'debug'
+    }
 
-    new Prompter(game).start();
+    new Prompter(game, options).boot()
 } catch (error) {
-    console.log(error);
-    console.log('You must provide a valid installed game');
-    process.exit();
+    console.log(error)
+    console.log('===> You must provide a valid installed tale')
+    process.exit()
 }
